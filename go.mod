6974f7b9569910ecf18ee37593ebbcfc8a365d87
Emanuel Quimper <quimperemanuel@gmail.com>
2020-07-25 10:16:46 -0400

    Create todos app

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-fiber-todos
+
+go 1.14
+
+require (
+	github.com/cosmtrek/air v1.12.1 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/gofiber/fiber v1.13.3
+	github.com/imdario/mergo v0.3.10 // indirect
+	github.com/klauspost/compress v1.10.10 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
+)
---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware"
+)
+
+type Todo struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []*Todo{
+	{Id: 1, Name: "Walk the dog", Completed: false},
+	{Id: 2, Name: "Walk the cat", Completed: false},
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+
+	app.Get("/", func(ctx *fiber.Ctx) {
+		ctx.Send("hello world")
+	})
+
+	SetupApiV1(app)
+
+	err := app.Listen(3000)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func SetupApiV1(app *fiber.App) {
+	v1 := app.Group("/v1")
+
+	SetupTodosRoutes(v1)
+}
+
+func SetupTodosRoutes(grp fiber.Router) {
+	todosRoutes := grp.Group("/todos")
+	todosRoutes.Get("/", GetTodos)
+	todosRoutes.Post("/", CreateTodo)
+	todosRoutes.Get("/:id", GetTodo)
+	todosRoutes.Delete("/:id", DeleteTodo)
+	todosRoutes.Patch("/:id", UpdateTodo)
+}
+
+func UpdateTodo(ctx *fiber.Ctx) {
+	type request struct {
+		Name      *string `json:"name"`
+		Completed *bool   `json:"completed"`
+	}
+
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	var body request
+	err = ctx.BodyParser(&body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse body",
+		})
+		return
+	}
+
+	var todo *Todo
+
+	for _, t := range todos {
+		if t.Id == id {
+			todo = t
+			break
+		}
+	}
+
+	if todo == nil {
+		ctx.Status(fiber.StatusNotFound)
+		return
+	}
+
+	if body.Name != nil {
+		todo.Name = *body.Name
+	}
+
+	if body.Completed != nil {
+		todo.Completed = *body.Completed
+	}
+
+	ctx.Status(fiber.StatusOK).JSON(todo)
+}
+
+func DeleteTodo(ctx *fiber.Ctx) {
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.Id == id {
+			todos = append(todos[0:i], todos[i+1:]...)
+			ctx.Status(fiber.StatusNoContent)
+			return
+		}
+	}
+
+	ctx.Status(fiber.StatusNotFound)
+}
+
+func GetTodo(ctx *fiber.Ctx) {
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	for _, todo := range todos {
+		if todo.Id == id {
+			ctx.Status(fiber.StatusOK).JSON(todo)
+			return
+		}
+	}
+
+	ctx.Status(fiber.StatusNotFound)
+}
+
+func CreateTodo(ctx *fiber.Ctx) {
+	type request struct {
+		Name string `json:"name"`
+	}
+
+	var body request
+
+	err := ctx.BodyParser(&body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse json",
+		})
+		return
+	}
+
+	todo := &Todo{
+		Id:        len(todos) + 1,
+		Name:      body.Name,
+		Completed: false,
+	}
+
+	todos = append(todos, todo)
+
+	ctx.Status(fiber.StatusCreated).JSON(todo)
+}
+
+func GetTodos(ctx *fiber.Ctx) {
+	ctx.Status(fiber.StatusOK).JSON(todos)
+}
